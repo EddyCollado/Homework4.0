1ccf9db752107779e31c91b04e269f103769ebdf
Eddy Collado <eddcollado@gmail.com>
2020-01-05 08:33:42 -0700

    added View Highscores nav tab

---
+++ questions.js
@@ -0,0 +1,16 @@
+// Store your questions as an array of objects in a separate file, questions.js, that follows this format:
+
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    ///etc.
+  ];
+  
