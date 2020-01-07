e2c9dff3977bbe444be0966ac67cde22b418829e
Eddy Collado <eddcollado@gmail.com>
2020-01-07 13:20:32 -0700

    updated questions.js, updated timer, updated html

--- questions.js
+++ questions.js
@@ -1,16 +1,33 @@
 // Store your questions as an array of objects in a separate file, questions.js, that follows this format:
-
-var questions = [
+   const myQuestions = [
     {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
+      question: "Commonly used data types DO NOT include:",
+      answers: {
+        a: "strings",
+        b: "booleans",
+        c: "alerts",
+        d: "numbers"
+      },
+      correctAnswer: "c"
     },
     {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+      question: "The condition in an if / else statement is enclosed within ____.",
+      answers: {
+        a: "quotes",
+        b: "curly brackets",
+        c: "parentheses",
+        d: "square brackets"
+      },
+      correctAnswer: "c"
     },
-    ///etc.
+    {
+      question: "Where is Waldo really?",
+      answers: {
+        a: "Antarctica",
+        b: "Exploring the Pacific Ocean",
+        c: "Sitting in a tree",
+        d: "Minding his own business, so stop asking"
+      },
+      correctAnswer: "d"
+    }
   ];
-  
--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 
 var timer = document.querySelector("#clockdiv");
 var viewHighscores = document.querySelector("body > ul > li > a");
+var secondsLeft = document.querySelector("#clockdiv > span");
+var startBtn = document.querySelector("#submit");
+
+var sec = 75;
+// fix the html/java conflict of the startBtn function 
+// var time = setInterval(myTimer, 1000)
+
+
+
+//startBtn.addEventListner("click", myTimer);
+
+function myTimer() {
+    secondsLeft.innerHTML = sec + "";
+    sec--;
+    if (sec == -1) {
+        clearInterval(time);
+        alert("Out of time!");
+    }
+}
+
+
 
 
 
